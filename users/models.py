5a97a9bc2b8205e94b7304e29cbc60f1159d6505
barackmaund1 <pmaundu49@gmail.co>
2020-06-17 19:42:59 +0300

    add educator authetication system

--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from PIL import Image
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(default='default.jpg', upload_to='profile_pics/')
+    contacts=models.CharField(max_length=50 ,blank=True,null=True)
+    bio=models.CharField(max_length=100,blank=True,null=True)
+    
+    def save_profile(self):
+        self.save()
+
+        
+    def __str__(self):
+        return f'{self.user.username} Profile'
+
+
--- users/views.py
+++ users/views.py
@@ -1,3 +1,42 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from .models import *
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('login')
+    else:
+        form = UserRegisterForm()
+    return render(request, 'users/register.html', {'form': form})
+
+@login_required
+def profile(request):
+    try:
+        profile = request.user.profile
+    except Profile.DoesNotExist:
+        profile = Profile(user=request.user)
+    if request.method == 'POST':
+        user_form = UserUpdateForm(request.POST, instance=request.user)
+        profile_form = ProfileUpdateForm(
+            request.POST or None, instance=profile)
+        if user_form.is_valid() and profile_form.is_valid():
+            user_form.save()
+            profile_form.save()
+            messages.success(
+                request, ('your profile was successfully updated!'))
+            return redirect('profile')
+        else:
+            messages.error(
+                request, ('There was an error updating your profile'))
+    else:
+        user_form = UserUpdateForm(instance=request.user)
+        profile_form = ProfileUpdateForm(instance=request.user.profile)
+    return render(request, 'users/profile.html', {'u_form': user_form, 'p_form': profile_form})    
