b54efed2c7204664abc67fad580f497ee350133c
ow-tech <wagwanwekon@outloo.com>
2020-06-18 20:05:20 +0300

    creation of email field for registration

--- student/views.py
+++ student/views.py
@@ -1,15 +1,16 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import StudentRegistrationForm
 
 def student_register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = StudentRegistrationForm(request.POST)
         if form.is_valid()
             form.save(0)
             username = form.cleaned_data.get('username')
             messages.success(request, f'Student Account created for {username}')
             return redirect('login')
     else:
-        form = UserCreationForm()
+        form = StudentRegistrationForm()
     return render(request, 'student/register.html',{'form':form})
