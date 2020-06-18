e81ed811b043524004ff89902ee3c28343d5c4cc
ow-tech <wagwanwekon@outloo.com>
2020-06-18 19:56:41 +0300

    added a save method to the user form

--- student/views.py
+++ student/views.py
@@ -5,7 +5,8 @@ from django.contrib import messages
 def student_register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
-        if form.is_valid():
+        if form.is_valid()
+            form.save(0)
             username = form.cleaned_data.get('username')
             messages.success(request, f'Student Account created for {username}')
             return redirect('login')
