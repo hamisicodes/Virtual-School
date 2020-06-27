ccde8a41a4e4032152eb4d57527fd25cc803eef9
ow-tech <wagwanwekon@outloo.com>
2020-06-28 00:13:53 +0300

    created a basic view and template for quiz

--- online_test/views.py
+++ online_test/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .models import Quiz
 
 # Create your views here.
+def list_of_quiz(request):
+    quizs = Quiz.objects.all()
+    context = {
+        "quizs":quizs
+    }
+    return render(request, 'online_test/quiz.html', context)
